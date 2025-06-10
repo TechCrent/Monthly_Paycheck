086e7f35a5d33f47c77c004c724f24da0a4cb6d4
TechCrent <ciicicrentsil@gmail.com>
2025-06-10 11:45:10 +0000

    Initiated Javascript program: Created a javascript file

---
+++ JavaScript/Monthly_Paycheck.js
@@ -0,0 +1,29 @@
+/*
+Challenge 1.0
+
+write a javascript program that calculates the monthly paycheck of a
+salesperson at a local department store. Every salesperson has
+a base salary. The salesperson also receives a bonus at the end
+of each month, based on the following criteria: if the
+salesperson has been with the store for five years or less, the
+bonus is $10 for each year that he or she has worked there. If
+the salesperson has been with the store for more than five years,
+the bonus is $20 for each year that he or she has worked there.
+The salesperson can earn an additional bonus as follows: if the
+total sales made by the salesperson for the month are at least
+$5, 000 but less than $10,000, he or she receives a 3% commission
+on the sale. If the total sales made by the salesperson for the
+month are at least $10, 000, he or she receives a 6% commission
+on the sale.
+*/
+
+//monthly paycheck
+//base salary
+// Bonus at the end of each month
+// if  salesperson < 5 years, bonus is $10 for each year
+// if salesperson > 5 years, bonus is $20 for each year
+// additional bonus:
+// if total sales >= $5,000 but < $10,000, 3% commission
+// if total sales >= $10,000, 6% commission
+
+
